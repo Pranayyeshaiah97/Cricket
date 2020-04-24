e131acd584bc253e0d733976b827be099397a226
Pranayyeshaiah97 <60440137+Pranayyeshaiah97@users.noreply.github.com>
2020-04-24 19:47:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById("signInButton").addEventListener("click", function signInButton(){
+    document.querySelector(".backGroundOnForm").style.display="flex";
+});
+
+document.getElementById("cancelButton").addEventListener("click", function cancelButton(){
+    document.querySelector(".backGroundOnForm").style.display="none";
+})
